0282af12cfc3538728d4c9dd84146f1fb3780d21
kelvin2002k <123325178+kelvin2002k@users.noreply.github.com>
2023-03-19 23:42:02 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>KitKat - My Favorite Candy</title>
+	
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+    <header>
+        <h1>KitKat - My Favorite Candy</h1>
+    </header>
+    <main>
+      
+        <section>
+            <h2>About KitKat</h2>
+            <p>KitKat is a chocolate-covered wafer bar that was created in England in 1935. It is now produced globally by Nestlé, and is one of the most popular candy bars in the world.</p>
+        </section>
+        <section>
+            <h2>My KitKat Collection</h2>
+            <div class="image-container">
+                <img src="images/R.png" alt="KitKat bar">
+                <img src="images/m.jpg" alt="KitKat Miniatures">
+                <img src="images/c.jpeg" alt="KitKat Chunky">
+            </div>
+        </section>
+    </main>
+</body>
+</html>
